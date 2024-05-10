f6a67501e6ef5ef723b66bb540d507f546838466
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-05-10 12:29:15 -0400

    Remove unused variables

--- src/components/add-to-cart/child-items/child-item/child-item.js
+++ src/components/add-to-cart/child-items/child-item/child-item.js
@@ -1,30 +1,18 @@
 /**
  * Internal dependencies
  */
-import { useChild } from '@context';
-
-
 import { default as GridItem } from '.././grid/child-item';
 import { default as TabularItem } from '.././tabular/child-item';
 
-
 const ChildItem = ( { loopClass } ) => {
-	const { childItem } = useChild();
 
-	const { name, images, catalog_visibility, stock_status } = childItem;
-
-	const firstImage = images.length ? images[ 0 ] : {};
-	const permalink =
-		catalog_visibility === 'hidden' || catalog_visibility === 'search'
-			? false
-			: childItem.permalink;
 	const isGridLayout =
 		WC_MNM_ADD_TO_CART_VARIATION_PARAMS.display_layout === 'grid';
 
 	return isGridLayout ? (
-		<GridItem childItem={childItem} />
+		<GridItem loopClass={loopClass} />
 	) : (
-		<TabularItem childItem={childItem} />
+		<TabularItem />
 	);
 };
 export default ChildItem;
